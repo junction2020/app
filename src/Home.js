c45a9e33df92aa3910962e1cd6d7c2efa0f39136
Gijs Wijngaard <gijswijngaard@live.nl>
2020-11-08 00:16:30 +0100

    Make rest boring

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import "./assets/main.css";
 import StickerPage from "./StickerPage";
-// import { stickers } from "./data";
+import { stickers } from "./data";
 import * as stickerManager from "./stickerManager";
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <StickerPage stickers={stickerManager.loadStickers()} />
+        <StickerPage stickers={stickers} />
       </div>
     );
   }
