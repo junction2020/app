6d54a22febd09559f890e74bd349f8bc8745b58a
Gijs Wijngaard <gijswijngaard@live.nl>
2020-11-07 20:50:35 +0100

    Add chart

--- src/Sticker.js
+++ src/Sticker.js
@@ -1,15 +1,18 @@
 import { Component } from 'react';
 import './assets/main.css'
 import {stickers} from "./data";
+import {barChartData, barChartOptions} from "./chart-data";
+import { Bar } from 'react-chartjs-2';
+
 
 class Sticker extends Component {
-  render() {
+    render() {
     return (
-    <div className="mt-20 container mx-auto">
-        {this.props.match.params.id} {stickers[this.props.match.params.id].brand.name}
-    </div>
-
-  )};
+        <div className="mt-20 container mx-auto">
+            <h3 className="text-xl font-bold mb-12">{stickers[this.props.match.params.id].brand.name}</h3>
+            <Bar data={barChartData(this.props.match.params.id)} options={barChartOptions} />
+        </div>
+    )};
 
 
 }
