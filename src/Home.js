c789fb76ef8172e945826428e6e3695b1f7a5cae
Gijs Wijngaard <gijswijngaard@live.nl>
2020-11-07 14:47:39 +0100

    More stickers

--- src/Home.js
+++ src/Home.js
@@ -7,13 +7,14 @@ class Home extends Component {
     return (
     <div>
         <div className="mt-20 container mx-auto">
-            <p>Welcome to our super cool dashboard!</p>
 
-            <h3>These are your stickers:</h3>
+            <h3 className="text-2xl">These are your stickers:</h3>
             <div className="flex flex-wrap">
                     {stickers.map((sticker, i) => (
                         <div key={i} className="h-auto items-stretch w-1/3 mb-12 h-12">
-                            <img className="object-fill w-full" src={sticker} alt={"unicorn"+i} />
+                            <div class="rounded-full h-auto w-100 p-5 ml-12 mr-12 flex items-center justify-center bg-gray-300">
+                                <img className="object-contain w-full" src={sticker} alt={"unicorn"+i} />
+                            </div>
                         </div>
                     ))}
             </div>
---
+++ src/Sticker.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import './assets/main.css'
+
+class Sticker extends Component {
+  render() {
+    return (
+    <div>
+        
+    </div>
+
+  )};
+
+
+}
+
+export default Sticker;
