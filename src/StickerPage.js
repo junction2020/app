ea3e416cddf9b20c75a8090b10bf3b25cbd8298a
mabergerx <maberger0811@gmail.com>
2020-11-07 21:30:57 +0100

    border changed

--- src/StickerPage.js
+++ src/StickerPage.js
@@ -6,7 +6,7 @@ class StickerPage extends Component {
   render() {
     return (
       <div className="mt-20 container mx-auto px-20 stickercard">
-        <div className="border-dotted border-8 border-purple-200 rounded-b-2xl rounded-t-2xl flex flex-wrap h-full pt-12">
+        <div className="border-solid border-8 border-green-300 rounded-b-2xl rounded-t-2xl flex flex-wrap h-full pt-12 stickercontainer">
           {stickers.map((sticker, i) => (
             <div key={i} className="h-auto items-stretch w-1/3 mb-12 h-3">
               <Link to={"sticker/" + i}>
