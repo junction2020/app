e068b8c7b88be14f693aea1d0d1ae92c27396a6b
Gijs Wijngaard <gijswijngaard@live.nl>
2020-11-08 03:15:30 +0100

    Buttons

--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,7 @@ class Home extends Component {
   state = {
     amountOfPages: 0,
     pageIndex: 0,
+    generationPossible: false,
     currentPage: [],
   };
 
@@ -37,13 +38,22 @@ class Home extends Component {
     console.log(this.state.currentPage);
     return (
       <div>
-        {this.state.pageIndex > 0 && (
-          <button onClick={this.previousPage}>Vorige</button>
-        )}
         <StickerPage stickers={this.state.currentPage} />
-        {this.state.pageIndex < this.state.amountOfPages - 1 && (
-          <button onClick={this.nextPage}>Volgende</button>
-        )}
+        <div class="flex justify-around mt-8">
+          {this.state.pageIndex > 0 ? (
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+            onClick={this.previousPage}>Previous</button>
+          ) : <span></span>}
+          {this.state.generationPossible ? (
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+            onClick={this.previousPage}>Choose goal</button>
+
+          ) : <span></span>}
+          {this.state.pageIndex < this.state.amountOfPages - 1 ? (
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" 
+            onClick={this.nextPage}>Next</button>
+          ) : <span></span>}
+        </div>
       </div>
     );
   }
