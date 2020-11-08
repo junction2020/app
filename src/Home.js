0bdfc4fb1e5957575c253e42795c9bc98fd0c22e
Richard Oosterlaken <richard.oosterlaken@gmail.com>
2020-11-08 04:51:06 +0100

    Add flair.

--- src/Home.js
+++ src/Home.js
@@ -37,25 +37,29 @@ class Home extends Component {
 
 
   render() {
-    let pledged = stickerManager.getPledgedPages();
-    console.log(this.state.currentPage);
+    let isPledged = stickerManager.isPagePledged(this.state.pageIndex);
     return (
       <div>
         <StickerPage stickers={this.state.currentPage} />
         <div class="flex justify-around mt-8">
           {this.state.pageIndex > 0 ? (
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-            onClick={this.previousPage}>Previous</button>
+            <button class="text-white bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+              onClick={this.previousPage}>Previous</button>
           ) : <span></span>}
           {!stickerManager.isPagePledged(this.state.currentPage) && this.state.currentPage.length === 15 ? (
             <Link to={"goals/" + this.state.pageIndex}>
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Choose goal</button>
+              <button
+                className={"text-white font-bold py-2 px-4 border border-blue-700 rounded " + (isPledged ? "bg-gray-500 hover:bg-gray-700 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-700 text-white ")}
+                disabled={isPledged}
+              >
+                {isPledged ? "Already pledged!" : "Choose goal"}
+              </button>
             </Link>
 
           ) : <span></span>}
           {this.state.pageIndex < this.state.amountOfPages - 1 ? (
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" 
-            onClick={this.nextPage}>Next</button>
+            <button className={"text-white bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"}
+              onClick={this.nextPage}>Next</button>
           ) : <span></span>}
         </div>
       </div>
