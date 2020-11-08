922a903eb63bf49bdcb529daf4eac0aeeaa5a330
mabergerx <maberger0811@gmail.com>
2020-11-08 01:59:08 +0100

    added pagination

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,38 @@
 import React, { Component } from "react";
 import "./assets/main.css";
 import StickerPage from "./StickerPage";
-import { stickers } from "./data";
 import * as stickerManager from "./stickerManager";
 
 class Home extends Component {
+  state = {
+    amountOfPages: 0,
+    pageIndex: 0,
+    currentPage: [],
+  };
+
+  componentDidMount() {
+    let amountOfPages = stickerManager.getAmountOfPages();
+    let pageIndex = amountOfPages - 1;
+    let currentPage = stickerManager.getStickersByPage(pageIndex);
+    this.setState({
+      amountOfPages,
+      pageIndex,
+      currentPage,
+    });
+  }
+
+  previousPage = () => {
+    let pageIndex = this.state.pageIndex - 1;
+    let currentPage = stickerManager.getStickersByPage(pageIndex);
+    this.setState({ pageIndex, currentPage });
+  };
+
   render() {
+    console.log(this.state.currentPage);
     return (
       <div>
-        <StickerPage stickers={stickerManager.loadStickers()} />
+        <button onClick={this.previousPage}>Vorige</button>
+        <StickerPage stickers={this.state.currentPage} />
       </div>
     );
   }
